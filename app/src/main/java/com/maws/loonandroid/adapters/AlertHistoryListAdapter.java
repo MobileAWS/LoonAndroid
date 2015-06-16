c7c40524c7a53e6e360dc149e9ccaef106649e45
jandresjc <jandresjc@gmail.com>
2015-06-16 13:17:42 -0500

    Removed multiple selection implementation from history.

--- app/src/main/java/com/maws/loonandroid/adapters/AlertHistoryListAdapter.java
+++ app/src/main/java/com/maws/loonandroid/adapters/AlertHistoryListAdapter.java
@@ -6,27 +6,19 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
 import com.maws.loonandroid.R;
-import com.maws.loonandroid.ifaces.MultipleSelectionAdapter;
 import com.maws.loonandroid.models.Alert;
 import com.maws.loonandroid.models.SensorService;
 import com.maws.loonandroid.util.Util;
-
-import org.droidparts.adapter.holder.ViewHolder;
-
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Andres Prada on 6/15/2015.
  */
-public class AlertHistoryListAdapter extends BaseAdapter implements MultipleSelectionAdapter<Alert> {
+public class AlertHistoryListAdapter extends BaseAdapter{
 
     private final Context context;
     private final List<Alert> items;
-    private List<Integer> selectedItems;
 
     static class ViewHolder {
         TextView descAlertHv, alertDismiss, alertDateHv, dismissDateHv, timeAlertHv, alertDateTV;
@@ -35,7 +27,6 @@ public class AlertHistoryListAdapter extends BaseAdapter implements MultipleSele
     public AlertHistoryListAdapter(List<Alert> items, Context context) {
         this.items = items;
         this.context = context;
-        this.selectedItems = new ArrayList<Integer>();
     }
 
     @Override
@@ -89,45 +80,6 @@ public class AlertHistoryListAdapter extends BaseAdapter implements MultipleSele
 
         }
         return convertView;
-    }
-
-    @Override
-    public List<Alert> getSelectedItems(){
-        ArrayList<Alert> selectedItemsToReturn = new ArrayList<Alert>();
-        for(Integer position: selectedItems){
-            selectedItemsToReturn.add(items.get(position));
-        }
-        return selectedItemsToReturn;
-    }
-
-    @Override
-    public void selectItem(int position) {
-
-    }
-
-    @Override
-    public void unselectItem(int position) {
-        selectedItems.remove( new Integer(position) );
-    }
-
-    @Override
-    public void selectAll() {
-        for(int i = 0; i < items.size(); i++){
-            if(!items.contains( new Integer(i) )){
-                selectItem(i);
-            }
-        }
-        notifyDataSetInvalidated();
-    }
-
-    @Override
-    public void toogleItem(int position){
-        if(selectedItems.contains( new Integer(position) )){
-            unselectItem(position);
-        }else{
-            selectItem(position);
-        }
-        notifyDataSetInvalidated();
     }
 
 }
